c1581e0b3d256520cb82a238d1c4916ba52ca19e
temple69 <utemple169@gmail.com>
2023-04-27 12:59:37 +0100

    created new files

--- 0x18-dynamic_libraries/README.md
+++
@@ -1,1 +0,0 @@
-Repository about dynamic libraries
--- test/testing.py
+++ test/testing.py
@@ -11,10 +11,54 @@ for  i in range(len(files)):
     repo.git.add(files[i])
     print(commits[i])
  """
+ 
 import os
-print(os.getcwd())
-files=['101-square','102-square','100-singly_linked_list','103-magic_class']
+files2=[]
+stringtxt=[]
+""" def ReadTextFromFile(filepath):
+    with open(filepath) as f:
+        lines = f.readlines()
+        return lines
+
+
+returnedFiles=ReadTextFromFile('./text.txt') 
+ #print(returnedFiles )
+
+def Readfile(arr):
+    for i in range(len(arr)):
+       stripped = arr[i].strip()
+       print(stripped )
+       if stripped.endswith("O"):
+           files.append(arr[i])
+
+Readfile(returnedFiles) 
+print(files)
+
 def createfile(arr):
     for i in range(len(arr)):
-        open(f'{os.getcwd()}/{arr[i]}.py','x')
-createfile(files)
+       open(f'{os.getcwd()}/{arr[i].strip()}','x')
+
+createfile(files) 
+  """
+
+
+""" for file in os.listdir():
+    if file.endswith('.txt'):
+        files2.append(file)
+stripped = files2[3:] """
+#converts elememts of list into string
+""" for i in range(len(contents)):
+    convertToString= str(contents[i])
+    stringtxt.append(convertToString)
+ """
+
+files=['0-hbtn_status','1-hbtn_header','10-my_github','100-github_commits','2-post_email','3-error_code','4-hbtn_status','5-hbtn_header','6-post_email','7-error_code','8-json_api']
+""" for i in range(1,6):
+    files.append(i)  """
+    
+
+def createfile(arr):
+    for i in range(len(arr)):
+       open(f'{os.getcwd()}/{arr[i]}.py','x')
+
+createfile(files)  
